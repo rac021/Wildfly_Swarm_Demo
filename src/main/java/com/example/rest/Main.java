83114e2a5edc4e4a55032da1dfa30b8414c44269
Yahiaoui Rachid <rac021@hotmail.com>
2016-08-28 13:35:51 +0200

    Update Main.java

--- src/main/java/com/example/rest/Main.java
+++ src/main/java/com/example/rest/Main.java
@@ -1,8 +1,9 @@
 
 package com.example.rest;
 
+import java.io.File;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
-import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.Swarm;
 import org.wildfly.swarm.jaxrs.JAXRSArchive;
 
 /**
@@ -13,12 +14,20 @@ public class Main {
     
      public static void main(String[] args) throws Exception {
  
-        Container container = new Container() ;
+        Swarm swarm = new Swarm() ;
  
         JAXRSArchive deployment = ShrinkWrap.create(JAXRSArchive.class, "my-app.war") ;
         deployment.addClass(RestApplication.class) ;
         deployment.addClass(HelloWorldEndpoint.class) ;
+        deployment.addClass(MyHystrixMetricsStreamServlet.class) ;
         deployment.addAllDependencies() ;
-        container.start().deploy(deployment) ;
+        
+        File file = new File("config.properties");
+        
+         if(file.exists()) {
+            deployment.addAsResource(file);
+         }
+         
+         swarm.start().deploy(deployment);
     }
 }
